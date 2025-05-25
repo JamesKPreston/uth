ff00ef710660a68fcebf8135d701a5e3729315a9
James <jamesp@spindance.com>
2025-05-25 14:21:31 -0400

    updated various logics , changed logic to evaluate ranks in my code due to bug in the poker evaluator library and now only use that to identify poker hands and rule tie breakers

--- lib/utility/poker_hands.dart
+++ lib/utility/poker_hands.dart
@@ -1,12 +1,12 @@
 enum PokerHandRank {
-  highCard,
-  onePair,
-  twoPair,
-  threeOfAKind,
-  straight,
-  flush,
-  fullHouse,
-  fourOfAKind,
-  straightFlush,
-  royalFlush
+  royalFlush, // 0 (best)
+  straightFlush, // 1
+  fourOfAKind, // 2
+  fullHouse, // 3
+  flush, // 4
+  straight, // 5
+  threeOfAKind, // 6
+  twoPair, // 7
+  onePair, // 8
+  highCard // 9 (worst)
 }
